96ecfeaeb165d5c55d91cc7ed82932512fc18ac2
khoiruuuls <2010631170092@student.unsika.ac.id>
2023-04-12 09:27:26 +0700

    khoirul commit

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
-const optionMenu = document.querySelector(".select-menu"),
-	selectBtn = optionMenu.querySelector(".select-btn"),
-	options = optionMenu.querySelectorAll(".option"),
-	sBtn_text = optionMenu.querySelector(".sBtn-text")
-
-selectBtn.addEventListener("click", () => optionMenu.classList.toggle("active"))
-
-options.forEach(option => {
-	option.addEventListener("click", () => {
-		optionMenu.classList.remove("active")
+let li = document.querySelectorAll(".faq-text li")
+for (var i = 0; i < li.length; i++) {
+	li[i].addEventListener("click", e => {
+		console.log(li)
+		let clickedLi
+		if (e.target.classList.contains("question-arrow")) {
+			clickedLi = e.target.parentElement
+		} else {
+			clickedLi = e.target.parentElement.parentElement
+		}
+		clickedLi.classList.toggle("showAnswer")
 	})
-})
+}
---
+++ js/tentang-kami.js
@@ -0,0 +1,229 @@
+// tentang kami
+const optionMenu1 = document.querySelector(".select-menu1"),
+	selectBtn1 = optionMenu1.querySelector(".select-btn1"),
+	options1 = optionMenu1.querySelectorAll(".option1"),
+	sBtn_text1 = optionMenu1.querySelector(".sBtn-text1")
+
+selectBtn1.addEventListener("click", () =>
+	optionMenu1.classList.toggle("active")
+)
+
+options1.forEach(option1 => {
+	option1.addEventListener("click", () => {
+		optionMenu1.classList.remove("active")
+	})
+})
+
+const optionMenu2 = document.querySelector(".select-menu2"),
+	selectBtn2 = optionMenu2.querySelector(".select-btn2"),
+	options2 = optionMenu2.querySelectorAll(".option2"),
+	sBtn_text2 = optionMenu2.querySelector(".sBtn-text2")
+
+selectBtn2.addEventListener("click", () =>
+	optionMenu2.classList.toggle("active")
+)
+
+options2.forEach(option2 => {
+	option2.addEventListener("click", () => {
+		optionMenu2.classList.remove("active")
+	})
+})
+
+const optionMenu3 = document.querySelector(".select-menu3"),
+	selectBtn3 = optionMenu3.querySelector(".select-btn3"),
+	options3 = optionMenu3.querySelectorAll(".option3"),
+	sBtn_text3 = optionMenu3.querySelector(".sBtn-text3")
+
+selectBtn3.addEventListener("click", () =>
+	optionMenu3.classList.toggle("active")
+)
+
+options3.forEach(option3 => {
+	option3.addEventListener("click", () => {
+		optionMenu3.classList.remove("active")
+	})
+})
+
+// program
+const optionMenu4 = document.querySelector(".select-menu4"),
+	selectBtn4 = optionMenu4.querySelector(".select-btn4"),
+	options4 = optionMenu4.querySelectorAll(".option4"),
+	sBtn_text4 = optionMenu4.querySelector(".sBtn-text4")
+
+selectBtn4.addEventListener("click", () =>
+	optionMenu4.classList.toggle("active")
+)
+
+options4.forEach(option4 => {
+	option4.addEventListener("click", () => {
+		optionMenu4.classList.remove("active")
+	})
+})
+
+const optionMenu5 = document.querySelector(".select-menu5"),
+	selectBtn5 = optionMenu5.querySelector(".select-btn5"),
+	options5 = optionMenu5.querySelectorAll(".option5"),
+	sBtn_text5 = optionMenu5.querySelector(".sBtn-text5")
+
+selectBtn5.addEventListener("click", () =>
+	optionMenu5.classList.toggle("active")
+)
+
+options5.forEach(option5 => {
+	option5.addEventListener("click", () => {
+		optionMenu5.classList.remove("active")
+	})
+})
+
+const optionMenu6 = document.querySelector(".select-menu6"),
+	selectBtn6 = optionMenu6.querySelector(".select-btn6"),
+	options6 = optionMenu6.querySelectorAll(".option6"),
+	sBtn_text6 = optionMenu6.querySelector(".sBtn-text6")
+
+selectBtn6.addEventListener("click", () =>
+	optionMenu6.classList.toggle("active")
+)
+
+options6.forEach(option6 => {
+	option6.addEventListener("click", () => {
+		optionMenu6.classList.remove("active")
+	})
+})
+
+// service
+const optionMenu7 = document.querySelector(".select-menu7"),
+	selectBtn7 = optionMenu7.querySelector(".select-btn7"),
+	options7 = optionMenu7.querySelectorAll(".option7"),
+	sBtn_text7 = optionMenu7.querySelector(".sBtn-text7")
+
+selectBtn7.addEventListener("click", () =>
+	optionMenu7.classList.toggle("active")
+)
+
+options7.forEach(option7 => {
+	option7.addEventListener("click", () => {
+		optionMenu7.classList.remove("active")
+	})
+})
+
+const optionMenu8 = document.querySelector(".select-menu8"),
+	selectBtn8 = optionMenu8.querySelector(".select-btn8"),
+	options8 = optionMenu8.querySelectorAll(".option8"),
+	sBtn_text8 = optionMenu8.querySelector(".sBtn-text8")
+
+selectBtn8.addEventListener("click", () =>
+	optionMenu8.classList.toggle("active")
+)
+
+options8.forEach(option8 => {
+	option8.addEventListener("click", () => {
+		optionMenu8.classList.remove("active")
+	})
+})
+
+const optionMenu9 = document.querySelector(".select-menu9"),
+	selectBtn9 = optionMenu9.querySelector(".select-btn9"),
+	options9 = optionMenu9.querySelectorAll(".option9"),
+	sBtn_text9 = optionMenu9.querySelector(".sBtn-text9")
+
+selectBtn9.addEventListener("click", () =>
+	optionMenu9.classList.toggle("active")
+)
+
+options9.forEach(option9 => {
+	option9.addEventListener("click", () => {
+		optionMenu9.classList.remove("active")
+	})
+})
+
+// pendaftarn
+const optionMenu10 = document.querySelector(".select-menu10"),
+	selectBtn10 = optionMenu10.querySelector(".select-btn10"),
+	options10 = optionMenu10.querySelectorAll(".option10"),
+	sBtn_text10 = optionMenu10.querySelector(".sBtn-text10")
+
+selectBtn10.addEventListener("click", () =>
+	optionMenu10.classList.toggle("active")
+)
+
+options10.forEach(option10 => {
+	option10.addEventListener("click", () => {
+		optionMenu10.classList.remove("active")
+	})
+})
+
+const optionMenu11 = document.querySelector(".select-menu11"),
+	selectBtn11 = optionMenu11.querySelector(".select-btn11"),
+	options11 = optionMenu11.querySelectorAll(".option11"),
+	sBtn_text11 = optionMenu11.querySelector(".sBtn-text11")
+
+selectBtn11.addEventListener("click", () =>
+	optionMenu11.classList.toggle("active")
+)
+
+options11.forEach(option11 => {
+	option11.addEventListener("click", () => {
+		optionMenu11.classList.remove("active")
+	})
+})
+
+const optionMenu12 = document.querySelector(".select-menu12"),
+	selectBtn12 = optionMenu12.querySelector(".select-btn12"),
+	options12 = optionMenu12.querySelectorAll(".option12"),
+	sBtn_text12 = optionMenu12.querySelector(".sBtn-text12")
+
+selectBtn12.addEventListener("click", () =>
+	optionMenu12.classList.toggle("active")
+)
+
+options12.forEach(option12 => {
+	option12.addEventListener("click", () => {
+		optionMenu12.classList.remove("active")
+	})
+})
+
+// pembayaran
+const optionMenu13 = document.querySelector(".select-menu13"),
+	selectBtn13 = optionMenu13.querySelector(".select-btn13"),
+	options13 = optionMenu13.querySelectorAll(".option13"),
+	sBtn_text13 = optionMenu13.querySelector(".sBtn-text13")
+
+selectBtn13.addEventListener("click", () =>
+	optionMenu13.classList.toggle("active")
+)
+
+options13.forEach(option13 => {
+	option13.addEventListener("click", () => {
+		optionMenu13.classList.remove("active")
+	})
+})
+
+const optionMenu14 = document.querySelector(".select-menu14"),
+	selectBtn14 = optionMenu14.querySelector(".select-btn14"),
+	options14 = optionMenu14.querySelectorAll(".option14"),
+	sBtn_text14 = optionMenu14.querySelector(".sBtn-text14")
+
+selectBtn14.addEventListener("click", () =>
+	optionMenu14.classList.toggle("active")
+)
+
+options14.forEach(option14 => {
+	option14.addEventListener("click", () => {
+		optionMenu14.classList.remove("active")
+	})
+})
+
+const optionMenu15 = document.querySelector(".select-menu15"),
+	selectBtn15 = optionMenu15.querySelector(".select-btn15"),
+	options15 = optionMenu15.querySelectorAll(".option15"),
+	sBtn_text15 = optionMenu15.querySelector(".sBtn-text15")
+
+selectBtn15.addEventListener("click", () =>
+	optionMenu15.classList.toggle("active")
+)
+
+options15.forEach(option15 => {
+	option15.addEventListener("click", () => {
+		optionMenu15.classList.remove("active")
+	})
+})
